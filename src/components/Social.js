2587104fe040083f2ef9a99307e32bd0867516d6
Paul Brady <pbrady74@gmail.com>
2022-05-13 01:56:53 -0700

    edit portfolio

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,9 +4,9 @@ const Social = () => {
             <h2>Socials</h2>
 
             <a href="" className="button">john@email.com</a>
-            <a href="https://twitter.com/" className="button">Twitter</a>
-            <a href="https://www.linkedin.com/" className="button">LinkedIn</a>
-            <a href="https://github.com/" className="button">GitHub</a>
+            <a href="https://twitter.com/paulbrady/" className="button">Twitter</a>
+            <a href="https://www.linkedin.com/in/paulbradyjr/" className="button">LinkedIn</a>
+            <a href="https://github.com/paulb7459/portfolio_template.git" className="button">GitHub</a>
         </div>
     );
 }
